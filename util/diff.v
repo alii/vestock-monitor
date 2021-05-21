6e946e9d8c26e72c80a10a91c07562b560823c1f
Alistair Smith <alistairsmith01@gmail.com>
2021-05-21 10:54:34 +0100

    update compare_product_caches

--- util/diff.v
+++ util/diff.v
@@ -2,15 +2,19 @@ module util
 
 import structs
 
-pub fn compare_product_caches(new_products []structs.MobileStockProduct, old_products []structs.MobileStockProduct) ? {
+pub fn compare_product_caches(new_products []structs.MobileStockProduct, old_products []structs.MobileStockProduct) []structs.MobileStockProduct {
+	mut restocked_products := []structs.MobileStockProduct{}
+
 	for new_product in new_products {
 		is_new := !product_exists_in_arr(new_product.id, old_products)
 
 		if is_new {
-			println('New restock $new_product.name')
+			restocked_products << new_product
 			continue
 		}
 	}
+
+	return restocked_products
 }
 
 fn product_exists_in_arr(id int, products []structs.MobileStockProduct) bool {
